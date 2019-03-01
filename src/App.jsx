77197089e811b1d9fc60eef2c4a68e8bb6f2cd5e
Erin Toth <erintoth@gmail.com>
2019-03-01 23:12:33 +0000

    Create nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './Nav.jsx';
 import axios from 'axios';
 
 class App extends Component {
@@ -36,7 +37,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <p>Hello</p>
+        <Nav />
       </div>
     );
   }
---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+export default class Nav extends Component {
+  render() {
+      return (
+        <nav className='navbar'>
+          <div><a href='/' className='navbar-brand'>Dungeon Wizard</a></div>
+        </nav>
+        )
+    } 
+    }
