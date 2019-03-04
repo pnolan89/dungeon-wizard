09491b7a833853a07b4a420184df92c8ec8bbc58
Matt Kelly <matt.r.kelly27@gmail.com>
2019-03-04 22:15:23 +0000

    test commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Home'
@@ -15,55 +14,6 @@ class App extends Component {
        <Route component={NotFound} />
       </Switch>
     </Router>
-=======
-import React, { Component } from 'react';
-import './App.css';
-import Nav from './Nav.jsx';
-import User from './User.jsx';
-import Campaign from './campaign.jsx';
-import CampaignIndex from './campaign-index.jsx';
-
-import axios from 'axios';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUser: {},
-      campaignData: {
-        campaign: {},
-        dm: {}
-      },
-      messages: []
-    };
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:3000/campaigns/4')
-      .then((response) => {
-        this.setState({
-          campaignData: {
-            campaign: response.data.campaign,
-            dm: response.data.dm
-          }
-        });
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Nav />
-        {/* <Campaign campaign={this.state.campaignData.campaign} dm={this.state.campaignData.dm}/> */}
-        <CampaignIndex campaign={this.state.campaignData.campaign} dm={this.state.campaignData.dm}/>
-      </div>
-    );
->>>>>>> feature/campaign-index
   }
 }
 
