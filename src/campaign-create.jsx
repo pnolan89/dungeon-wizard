5a5392f7d128ae5f8d3f08f126b5834ee94876d1
Matt Kelly <matt.r.kelly27@gmail.com>
2019-03-04 22:12:53 +0000

    first commit

---
+++ src/campaign-create.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import './campaign-index.css';
+
+class CampaignIndex extends Component {
+    render() {
+        return(
+            <div className="Campaign-Index">
+                <div className="Campaign-Card">
+                    <div className="Campaign-Card-Details">
+                        <h1>The Great Campaign</h1>
+                        <p>Dungeon Master: ME</p>
+                        <p>Description: It's so good omg.</p>
+                        <p>Playing Style: Easy Peasy</p>
+                        <h3>JOIN!</h3>
+                    </div>
+                    <div className="Campaign-Index-Image">
+                        <img src="https://bit.ly/2C3tnvb" />
+                    </div>
+
+
+                </div>
+            
+                
+            </div>
+
+           
+        );
+    }
+}
+export default CampaignIndex;
