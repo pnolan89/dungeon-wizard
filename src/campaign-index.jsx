e7a8e640be8a6f635b2a8c6997a6ce83031fca5e
pnolan89 <petenol@gmail.com>
2019-03-06 22:15:45 +0000

    change campaign index filter function to take in parameters from state

--- src/campaign-index.jsx
+++ src/campaign-index.jsx
@@ -8,12 +8,16 @@ class CampaignIndex extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        filters: {
+            exp_level: "",
+            playing_style: ""
+        }
     };
 }
 
 
 componentDidMount() {
-  axios.get(`http://localhost:3000/campaigns/`)
+  axios.get('http://localhost:3000/campaigns/')
   .then ((response) => {
       this.setState({
           campaigns: response.data
@@ -25,7 +29,13 @@ componentDidMount() {
 }
 
 filterCampaigns(campaigns) {
-    let filteredList = campaigns.filter((campaign) => campaign.campaign.playing_style === "story-focused");
+    let filteredList = campaigns;
+    if (this.state.filters.exp_level) {
+        filteredList = filteredList.filter((campaign) => campaign.campaign.exp_level === this.state.filters.exp_level);
+    }
+    if (this.state.filters.playing_style) {
+        filteredList = filteredList.filter((campaign) => campaign.campaign.playing_style === this.state.filters.playing_style);
+    }
     return filteredList;
 }
 
@@ -33,22 +43,21 @@ filterCampaigns(campaigns) {
 getCampaignData() {
   if (this.state.campaigns) {
     let filteredList = this.filterCampaigns(this.state.campaigns);
-    console.log("FILTEREDLIST: ", filteredList);
-      const campaigns = filteredList.map((campaign) => {
-        return(
-          <div className="Campaign-Index">
-          <div className="Campaign-Card">
-            <div className="Campaign-Card-Details">
-                <h1><Link to={`/campaigns/${campaign.campaign.id}`}>{campaign.campaign.name}</Link></h1>
-                <p>Dungeon Master: {campaign.dm.name}</p>
-                <p>Description: {campaign.campaign.description}</p>
-                <p>Location: {campaign.campaign.location}</p>
-            </div>
-            <div className="Campaign-Index-Image">
-                <img src="https://bit.ly/2XE42RH" />
-            </div>
-          </div>
-          </div>
+    const campaigns = filteredList.map((campaign) => {
+      return(
+        <div className="Campaign-Index">
+        <div className="Campaign-Card">
+        <div className="Campaign-Card-Details">
+            <h1><Link to={`/campaigns/${campaign.campaign.id}`}>{campaign.campaign.name}</Link></h1>
+            <p>Dungeon Master: {campaign.dm.name}</p>
+            <p>Description: {campaign.campaign.description}</p>
+            <p>Location: {campaign.campaign.location}</p>
+        </div>
+        <div className="Campaign-Index-Image">
+            <img src="https://bit.ly/2XE42RH" />
+        </div>
+        </div>
+        </div>
         )
       })
       return campaigns;
