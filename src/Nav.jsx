a8af7ba361a2e79c9cb7be92e112793fa75ad036
pnolan89 <petenol@gmail.com>
2019-03-06 03:19:04 +0000

    add login indicator span to nav component

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -8,6 +8,7 @@ export default class Nav extends Component {
         <nav className='navbar'>
           <span className='logo'><a href='/' className='navbar-brand'><h1>DuNgeOn WiZarD</h1></a> </span>
           <span className='links'> <Link to="/users/">Users</Link> | <Link to="/campaigns/">Campaigns</Link> | <Link to="/register/">Register</Link></span>
+          <span>{localStorage.currentUser}</span>
         </nav>
         )
     }
