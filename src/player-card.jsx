47c7b0410da7bba9ca922e54b77cacc87b8c401e
pnolan89 <petenol@gmail.com>
2019-03-10 21:26:14 +0000

    player remove card appears dynamically for players as well as ddms

--- src/player-card.jsx
+++ src/player-card.jsx
@@ -5,25 +5,31 @@ import { Link } from "react-router-dom";
 class PlayerCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.onClick = this.onClick.bind(this);
   }
 
   renderEmail() {
     if (this.props.isPlayer || this.props.isDM) {
       return (
         <React.Fragment>
-        <h4>Email: <span className="card-value">{this.props.playerInfo.email}</span></h4>
+        <h4>Email: <span className="card-value">{this.props.player.email}</span></h4>
         </React.Fragment>
       );
     }
   }
 
+  onClick() {
+    this.props.handlePlayerRemove(this.props.player.id)
+  }
+
   renderRemoveBtn() {
     if (this.props.isDM) {
       return (
-        <button className="player-delete-btn">Remove</button>
+        <button onClick={this.onClick} className="player-delete-btn">Remove</button>
+      )
+    } else if (this.props.player.id === parseInt(localStorage.user_id)) {
+      return (
+        <button onClick={this.onClick} className="player-delete-btn">Resign</button>
       )
     }
   }
@@ -32,27 +38,22 @@ class PlayerCard extends Component {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  componentDidMount() {
-    console.log('PLAYER CARD REACHED: ', this.props.playerInfo)
-  }
-
   render() {
     return(
-      <Link to={`/users/${this.props.playerInfo.id}`}>
       <div className="player">
         <div className="player-info">
-          <h3>{this.props.playerInfo.name}</h3>
+          <Link to={`/users/${this.props.player.id}`}>
+            <h3>{this.props.player.name}</h3>
+          </Link>
           {this.renderEmail()}
-          <h4>Experience level: <span className="card-value">{this.capitalize(this.props.playerInfo.exp_level)}</span></h4>
-          <h4>Play style: <span className="card-value">{this.capitalize(this.props.playerInfo.playing_style)}</span></h4>
-
+          <h4>Experience level: <span className="card-value">{this.capitalize(this.props.player.exp_level)}</span></h4>
+          <h4>Play style: <span className="card-value">{this.capitalize(this.props.player.playing_style)}</span></h4>
         </div>
         <div className="player-badges">
-          <img src={this.props.playerInfo.avatar} />
+          <img src={this.props.player.avatar} />
           {this.renderRemoveBtn()}
         </div>
       </div>
-      </Link>
     )
   }
 }
