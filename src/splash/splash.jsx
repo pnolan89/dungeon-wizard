fb6748b08f924195020275844a24e484fe2f871c
pnolan89 <petenol@gmail.com>
2019-03-14 21:59:51 +0000

    grammar

--- src/splash/splash.jsx
+++ src/splash/splash.jsx
@@ -11,21 +11,18 @@ class Splash extends Component {
       redirect: false,
     };
 }
-   
+
     render() {
         return(
 
                 <div className="splash">
-               
+
                 <h1>bringing adventurers together</h1>
 
                 <div className="splash-details">
 
                 <div className="image">
-                {/* <img src="https://i.imgur.com/JKNZONw.png"/> */}
                 <img src="https://i.imgur.com/JDIWMnS.png"/>
-
-                {/* <img src="https://i.imgur.com/TFXcMoU.png" /> */}
 
                 </div>
 
@@ -35,23 +32,23 @@ class Splash extends Component {
                 <h3>REGISTER</h3>
                 </Link>
                 </div>
-               
+
                 <div className="login">
-                <h2>welcome back traveller...</h2>          
+                <h2>welcome back, traveller...</h2>
                 <Link to="/login/">
                 <h3>LOGIN</h3>
                 </Link>
                 </div>
 
                 <div className="campaigns" >
-                <h2>your adventures await!</h2>  
+                <h2>your adventures await!</h2>
                 <Link to="/campaigns/">
                 <h3>CAMPAIGNS</h3>
                 </Link>
                 </div>
 
                 </div>
-   
+
                 </div>
         );
     }
